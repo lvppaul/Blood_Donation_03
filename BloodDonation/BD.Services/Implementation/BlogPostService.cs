41a7de8a8d832624e9402db3bd49997752c184ea
NguyenDucThang201204 <thangnguyenduc2004@gmail.com>
2025-06-11 01:55:47 +0700

    AddScope in Program.cs

--- BloodDonation/BD.Services/Implementation/BlogPostService.cs
+++ BloodDonation/BD.Services/Implementation/BlogPostService.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BD.Repositories.Models.DTOs.Requests;
+using BD.Repositories.Models.Entities;
+using BD.Services.Interfaces;
 
 namespace BD.Services.Implementation
 {
-    public class BlogPostService
+    public class BlogPostService : IBlogPostService
     {
+        public Task<BlogPost> AddAsync(BlogPostRequest post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeletePostAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<BlogPost>> GetAllPostsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BlogPost> GetPostByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BlogPost> UpdatePostAsync(int id, BlogPostRequest post)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
