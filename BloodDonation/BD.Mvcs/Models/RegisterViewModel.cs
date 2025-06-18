098ec8c7afbd5c102587a15123693b7bea8cfc1f
lvppaul <levinhphat790@gmail.com>
2025-06-18 10:27:26 +0700

    Add project Razor pages

---
+++ BloodDonation/BD.Mvcs/Models/RegisterViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BD.Mvcs.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
