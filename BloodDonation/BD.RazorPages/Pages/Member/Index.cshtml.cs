ca7caa4ef377e0d3a00c9665506f9d7ce54b986d
lvppaul <levinhphat790@gmail.com>
2025-06-18 11:50:05 +0700

    Init architecture at Razor Pages Project

---
+++ BloodDonation/BD.RazorPages/Pages/Member/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BD.RazorPages.Pages.Member
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ BloodDonation/BD.RazorPages/Pages/Public/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BD.RazorPages.Pages.Public
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
