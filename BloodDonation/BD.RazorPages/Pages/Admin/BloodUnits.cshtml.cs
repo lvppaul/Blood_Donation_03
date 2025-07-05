b0da131a766d51a66c0e890b536d7a147914560c
lvppaul <levinhphat790@gmail.com>
2025-07-05 11:36:41 +0700

    Blood Inventory
    1. rework UI, add logic
    2. generate GetAllBloodInventoriesByBloodTypeAsync,TotalBloodInventoriesByBloodTypeAsync, GetBloodTypeOverviewAsync
    3. generate Blood Type Enum
    4. generate Blood Type Overview

--- BloodDonation/BD.RazorPages/Pages/Admin/BloodUnits.cshtml.cs
+++ BloodDonation/BD.RazorPages/Pages/Admin/BloodUnits.cshtml.cs
@@ -1,11 +1,28 @@
+using BD.Repositories.Models.DTOs.Responses;
+using BD.Repositories.Models.Entities;
+using BD.Services.Interfaces;
+using BD.Services.Others;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace BD.RazorPages.Pages.Admin
 {
     public class BloodUnitsModel : PageModel
     {
-        public void OnGet()
+        private IBloodInventoryService _bloodInventory;
+        public IEnumerable<BloodInventoryResponse> BloodUnits { get; set; }
+        public IEnumerable<BloodTypeOverview> BloodTypeOverviews { get; set; }
+
+        public int TotalCountA { get; set; }
+        public BloodUnitsModel(IBloodInventoryService bloodInventory)
         {
+            _bloodInventory = bloodInventory;
+        }
+        public async Task OnGetAsync()
+        {
+            BloodUnits = await _bloodInventory.GetAllAsync();
+            BloodTypeOverviews = await _bloodInventory.GetBloodTypeOverviewAsync();
         }
     }
+
 }
