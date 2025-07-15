008aabf2407d0ce04b2678deb95463a385e04a43
Layze04 <layzegot@gmail.com>
2025-07-15 21:15:40 +0700

    - fix ui - add term, privacy, about us pages

---
+++ BloodDonation/BD.RazorPages/Pages/PrivacyPolicy.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BD.RazorPages.Pages
+{
+    public class PrivacyPolicyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
